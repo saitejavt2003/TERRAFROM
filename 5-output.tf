6b6751e3b6078c7a5099f1c11016de064bae2ab9
saiteja <saitejavt20@GMAIL.COM>
2025-03-12 13:27:48 +0530

    added jenkinsi

--- 5-output.tf
+++ 5-output.tf
@@ -1,11 +1,7 @@
-output "vm_instance_name" {
+output "vm_instance" {
   value = google_compute_instance.vm_instance.name
 }
 
-output "sql_database_instance" {
-  value = google_sql_database_instance.db_instance.name
+output "bucket" {
+  value = google_storage_bucket.bucket.name
 }
-
-#output "storage_bucket_name" {
-#  value = google_storage_bucket.bucket.name
-#}
